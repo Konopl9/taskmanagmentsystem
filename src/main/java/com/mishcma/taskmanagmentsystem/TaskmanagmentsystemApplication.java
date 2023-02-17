c363284454cb5940312bb1463f0607695e537bcf
Maksym <maksimus2998@gmail.com>
2023-02-17 10:27:58 +0100

    initial commit

---
+++ src/main/java/com/mishcma/taskmanagmentsystem/TaskmanagmentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.mishcma.taskmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagmentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagmentsystemApplication.class, args);
+	}
+
+}
