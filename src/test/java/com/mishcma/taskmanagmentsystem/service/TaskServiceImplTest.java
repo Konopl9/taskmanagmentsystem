3b132825937bbea07e3847b74ea66e75722c5d33
Maksym <maksimus2998@gmail.com>
2023-03-12 15:16:55 +0100

    added JUnit tests for services #Closes 9

---
+++ src/test/java/com/mishcma/taskmanagmentsystem/service/TaskServiceImplTest.java
@@ -0,0 +1,110 @@
+package com.mishcma.taskmanagmentsystem.service;
+
+import com.mishcma.taskmanagmentsystem.entity.Task;
+import com.mishcma.taskmanagmentsystem.exception.EntityNotFoundException;
+import com.mishcma.taskmanagmentsystem.repository.TaskRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class TaskServiceImplTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @InjectMocks
+    private TaskServiceImpl taskService;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetTaskById() {
+        Task task = new Task(1L, "Test Task", "This is a test task", (short) 1, "OPEN", null, null);
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+
+        Task result = taskService.getTaskById(1L);
+
+        verify(taskRepository, times(1)).findById(1L);
+        Assertions.assertEquals(task, result);
+    }
+
+    @Test
+    public void testGetTaskByIdNotFound() {
+        when(taskRepository.findById(any())).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> taskService.getTaskById(1L));
+    }
+
+    @Test
+    public void testGetTasks() {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(1L, "Test Task 1", "This is test task 1", (short) 1, "OPEN", null, null));
+        tasks.add(new Task(2L, "Test Task 2", "This is test task 2", (short) 2, "CLOSED", null, null));
+        when(taskRepository.findAll()).thenReturn(tasks);
+
+        List<Task> result = taskService.getTasks();
+
+        verify(taskRepository, times(1)).findAll();
+        Assertions.assertEquals(tasks, result);
+    }
+
+    @Test
+    public void testCreateTask() {
+        Task task = new Task(1L, "Test Task", "This is a test task", (short) 1, "OPEN", null, null);
+        when(taskRepository.save(any())).thenReturn(task);
+
+        Task result = taskService.createTask(task);
+
+        verify(taskRepository, times(1)).save(task);
+        Assertions.assertEquals(task, result);
+    }
+
+    @Test
+    public void testUpdateTaskStatus() {
+        Task oldTask = new Task(1L, "Test Task", "This is a test task", (short) 1, "To do", null, null);
+        Task updatedTask = new Task(1L, "Test Task", "This is a test task", (short) 1, "In progress", null, null);
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(oldTask));
+        when(taskRepository.save(any())).thenReturn(updatedTask);
+
+        Task result = taskService.updateTaskStatus(1L, "In progress");
+
+        verify(taskRepository, times(1)).findById(1L);
+        verify(taskRepository, times(1)).save(any(Task.class));
+        Assertions.assertEquals(updatedTask, result);
+    }
+
+    @Test
+    public void testUpdateTaskPriority() {
+        Task oldTask = new Task(1L, "Test Task", "This is a test task", (short) 1, "To do", null, null);
+        Task updatedTask = new Task(1L, "Test Task", "This is a test task", (short) 1, "In progress", null, null);
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(oldTask));
+        when(taskRepository.save(any())).thenReturn(updatedTask);
+
+        Task result = taskService.updateTaskPriority(1L, (short) 2);
+
+        verify(taskRepository, times(1)).findById(1L);
+        verify(taskRepository, times(1)).save(any(Task.class));
+        Assertions.assertEquals(updatedTask, result);
+    }
+
+    @Test
+    public void testDeleteTask() {
+        taskService.deleteTask(1L);
+        verify(taskRepository, times(1)).deleteById(1L);
+    }
+}
---
+++ src/test/java/com/mishcma/taskmanagmentsystem/service/UserServiceImplTest.java
@@ -0,0 +1,112 @@
+package com.mishcma.taskmanagmentsystem.service;
+
+import com.mishcma.taskmanagmentsystem.entity.Task;
+import com.mishcma.taskmanagmentsystem.entity.User;
+import com.mishcma.taskmanagmentsystem.repository.TaskRepository;
+import com.mishcma.taskmanagmentsystem.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getUserById() {
+        User user = new User(1L, "username", "password", "email", new ArrayList<>());
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        User result = userService.getUserById(1L);
+
+        verify(userRepository, times(1)).findById(1L);
+        assertEquals(user, result);
+    }
+
+    @Test
+    void getUsers() {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(1L, "username1", "password1", "email1", new ArrayList<>()));
+        userList.add(new User(2L, "username2", "password2", "email2", new ArrayList<>()));
+        when(userRepository.findAll()).thenReturn(userList);
+
+        List<User> result = userService.getUsers();
+
+        verify(userRepository, times(1)).findAll();
+        assertEquals(userList, result);
+    }
+
+    @Test
+    void createUser() {
+        User user = new User(1L, "username", "password", "email", new ArrayList<>());
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        User result = userService.createUser(user);
+
+        verify(userRepository, times(1)).save(user);
+        assertEquals(user, result);
+    }
+
+    @Test
+    void updateUserEmail() {
+        User oldUser = new User(1L, "username", "password", "email1", new ArrayList<>());
+        User newUser = new User(1L, "username", "password", "email2", new ArrayList<>());
+        User updatedUser = new User(1L, "username", "password", "email2", new ArrayList<>());
+        when(userRepository.findById(1L)).thenReturn(Optional.of(oldUser));
+        when(userRepository.save(any(User.class))).thenReturn(updatedUser);
+
+        User result = userService.updateUserEmail(newUser);
+
+        verify(userRepository, times(1)).findById(1L);
+        verify(userRepository, times(1)).save(any(User.class));
+        assertEquals(updatedUser, result);
+    }
+
+    @Test
+    void deleteUser() {
+        userService.deleteUser(1L);
+
+        verify(userRepository, times(1)).deleteById(1L);
+    }
+
+    @Test
+
+    public void testAssignTaskToUserSuccess() {
+        User user = new User(1L, "testuser", "password", "testuser@test.com", Collections.emptyList());
+        Task task = new Task(1L, "Test task", "Test task description", (short) 1, "TODO", null, null);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        when(taskRepository.save(any(Task.class))).thenReturn(task);
+
+        User assignedUser = userService.assignTaskToUser(1L, 1L);
+
+        assertNotNull(assignedUser);
+
+        verify(userRepository, times(2)).findById(1L);
+        verify(taskRepository, times(1)).findById(1L);
+    }
+}
